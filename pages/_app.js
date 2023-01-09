013349f2dceecfcdea17d012729865e83494424f
kali2498 <103638485+kali2498@users.noreply.github.com>
2023-01-09 14:32:01 +0530

    add file

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { ChakraProvider } from "@chakra-ui/react";
+import Navbar from "./navbar";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider>
+      <Navbar />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
+
+export default MyApp;
